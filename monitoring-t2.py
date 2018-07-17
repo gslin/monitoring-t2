0c9fc4ac19745e61c45019d96b50ee9301cd9820
Gea-Suan Lin <gslin@gslin.org>
2018-07-18 01:11:16 +0800

    Get t2.* then foreach them.

--- monitoring-t2.py
+++ monitoring-t2.py
@@ -3,8 +3,19 @@
 import boto3
 
 def lambda_handler(event, context):
-    cw = boto3.client('cloudwatch')
     ec2 = boto3.client('ec2')
+
+    instances = ec2.describe_instances(Filters=[{
+        'Name': 'instance-type',
+        'Values': ['t2.*'],
+    }])
+
+    for instance in instances['Reservations']:
+        for i in instance['Instances']:
+            validate_instance(i['InstanceId'])
+
+def validate_instance(id):
+    pass
 
 if __name__ == '__main__':
     lambda_handler(None, None)
