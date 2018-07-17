6463c17a62b3e93c1bfa71a4ebf5da9a9ff760f1
Gea-Suan Lin <gslin@gslin.org>
2018-07-17 16:11:24 +0800

    Get all t2.* instances.

--- monitoring-t2.py
+++ monitoring-t2.py
@@ -6,6 +6,10 @@ import time
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
+    instances = ec2.describe_instances(Filters=[{
+        'Name': 'instance-type',
+        'Values': ['t2.*'],
+    }])
 
 if __name__ == '__main__':
     lambda_handler(None, None)
