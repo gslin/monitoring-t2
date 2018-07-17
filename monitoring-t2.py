5e7ac557278ce343972b24132f818060e3247324
Gea-Suan Lin <gslin@gslin.org>
2018-07-17 16:18:19 +0800

    Change to use cloudwatch object.

--- monitoring-t2.py
+++ monitoring-t2.py
@@ -5,11 +5,7 @@ import json
 import time
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
-    instances = ec2.describe_instances(Filters=[{
-        'Name': 'instance-type',
-        'Values': ['t2.*'],
-    }])
+    cw = boto3.client('cloudwatch')
 
 if __name__ == '__main__':
     lambda_handler(None, None)
