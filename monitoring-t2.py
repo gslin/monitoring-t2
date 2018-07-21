cac5d27e8937bd05065e07dfaeadc10a70cc475a
Gea-Suan Lin <gslin@gslin.org>
2018-07-21 17:08:54 +0800

    Change to class.

--- monitoring-t2.py
+++ monitoring-t2.py
@@ -5,42 +5,48 @@ import datetime
 
 CLOUDWATCH_PERIOD = 300
 
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
+class monitor_t2:
+    def lambda_handler(self, event, context):
+        ec2 = boto3.client('ec2')
 
-    instances = ec2.describe_instances(Filters=[{
-        'Name': 'instance-type',
-        'Values': ['t2.*'],
-    }])
+        instances = ec2.describe_instances(Filters=[{
+            'Name': 'instance-type',
+            'Values': ['t2.*'],
+        }])
 
-    for instance in instances['Reservations']:
-        for i in instance['Instances']:
-            validate_instance(i['InstanceId'])
+        for instance in instances['Reservations']:
+            for i in instance['Instances']:
+                self.validate_instance(i['InstanceId'])
 
-def validate_instance(id):
-    cw = boto3.client('cloudwatch')
+    def validate_instance(self, id):
+        cw = boto3.client('cloudwatch')
 
-    now = datetime.datetime.utcnow()
-    res = cw.get_metric_statistics(
-        Namespace='AWS/EC2',
-        MetricName='CPUCreditBalance',
-        Dimensions=[{
-            'Name': 'InstanceId',
-            'Value': id,
-        }],
-        StartTime=now - datetime.timedelta(seconds=CLOUDWATCH_PERIOD),
-        EndTime=now,
-        Period=CLOUDWATCH_PERIOD,
-        Statistics=['Average'],
-    )
+        now = datetime.datetime.utcnow()
+        res = cw.get_metric_statistics(
+            Namespace='AWS/EC2',
+            MetricName='CPUCreditBalance',
+            Dimensions=[{
+                'Name': 'InstanceId',
+                'Value': id,
+            }],
+            StartTime=now - datetime.timedelta(seconds=CLOUDWATCH_PERIOD),
+            EndTime=now,
+            Period=CLOUDWATCH_PERIOD,
+            Statistics=['Average'],
+        )
 
-    # Now res['Datapoints'][0]['Average'] is the result.
+        sns = boto3.client('sns')
 
-    try:
-        if res['Datapoints'][0]['Average'] < 10:
+        try:
+            print(res)
+            if res['Datapoints'][0]['Average'] < 10:
+                pass
+        except IndexError:
             pass
-    except IndexError:
-        pass
+
+def lambda_handler(event, context):
+    m = monitor_t2()
+    m.lambda_handler(event, context)
 
 if __name__ == '__main__':
     lambda_handler(None, None)
